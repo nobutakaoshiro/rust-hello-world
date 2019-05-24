250830b0c2986894083591e5c215c380d8cf5ed2
Nobutaka Oshiro <3583286+nobutakaoshiro@users.noreply.github.com>
2019-05-24 14:13:46 +0900

    concat strings exmaple

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {  
-    println!("Hello, world!");
+fn main() {
+    let name = "Taro";
+    // dbg!(name);
+    let result = build_message(&name);
+    // dbg!(result);
+    println!("{}", result);
+}
+
+fn build_message(name: &str) -> String {
+    format!("{}, {}!", "Hello", name)
 }
